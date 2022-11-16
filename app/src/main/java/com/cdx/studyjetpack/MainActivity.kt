329fe525866cbe8ea7804de98d26ffacd661a288
chendexiang <937682896@qq.com>
2022-11-16 10:29:09 +0800

    Initial commit

---
+++ app/src/main/java/com/cdx/studyjetpack/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.cdx.studyjetpack
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.findNavController
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_main2)
+    }
+}
